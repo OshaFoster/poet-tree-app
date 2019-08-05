ffdd808c0a9b921b4e73c921cf485c53cf653194
Osha Foster <oshafoster@hotmail.com>
2019-08-04 21:54:17 -0600

    created placeholder screens (with custom color backgrounds for flaire)

---
+++ App/Containers/Themes/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import { connect } from 'react-redux'
+// Add Actions - replace 'Your' with whatever your reducer is called :)
+// import YourActions from '../Redux/YourRedux'
+
+// Styles
+import { ROOT } from './styles'
+
+class Themes extends Component {
+  render() {
+    return (
+      <View style={ROOT}>
+        <Text>Themes</Text>
+      </View>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Themes)
--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,11 +1,12 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation'
 import LaunchScreen from '../Containers/LaunchScreen'
+import About from '../Containers/About'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = createStackNavigator({
-  LaunchScreen: { screen: LaunchScreen }
+  LaunchScreen: { screen: About }
 }, {
   // Default config for all screens
   headerMode: 'none',
