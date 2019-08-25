1c97443d2e3a93fea8823f1ee930875fa547b872
Kit Masaracchia <kit.masaracchia@gmail.com>
2019-08-25 14:18:45 -0600

    fixed pod issue; build still broken

--- android/app/src/main/java/com/poettreeapp/MainApplication.java
+++ android/app/src/main/java/com/poettreeapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.poettreeapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeConfigPackage(),
             new AsyncStoragePackage(),
             new RNDeviceInfo(),
             new RNGestureHandlerPackage()
