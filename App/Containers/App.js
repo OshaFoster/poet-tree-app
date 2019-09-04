f75f54be481721daea5a5e9bd28b5a39322f5bd6
Kit Masaracchia <kit.masaracchia@gmail.com>
2019-09-04 01:10:55 -0600

    properly link react-native-firebase and configure podfile; logging words from the database

--- App/Containers/App.js
+++ App/Containers/App.js
@@ -19,31 +19,17 @@ const store = createStore();
  * We separate like this to play nice with React Native's hot reloading.
  */
 
-// import firebase from "firebase/app";
-// import "@firebase/firestore";
-// // import PropTypes from 'prop-types';
-// // import styled from 'styled-components';
-
-// firebase.initializeApp({
-//   apiKey: "AIzaSyBBcpAywBVDPhG-4vehBvIbHPtpkeQNnIU",
-//   authDomain: "poet-tree.firebaseapp.com",
-//   databaseURL: "https://poet-tree.firebaseio.com",
-//   projectId: "poet-tree",
-//   storageBucket: "poet-tree.appspot.com",
-//   messagingSenderId: "721416476712",
-//   appId: "1:721416476712:web:2679f51b047f4d87"
-// });
-
-// export const db = firebase.firestore();
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
 
 class App extends Component {
   componentDidMount = async () => {
-    // const documentSnapshot = await firestore()
-    //   .collection("users")
-    //   .doc("vYzrR2IndP4t13fBiNqa")
-    //   .get();
+    const documentSnapshot = await firestore()
+      .collection("words")
+      .doc("vYzrR2IndP4t13fBiNqa")
+      .get();
 
-    // console.log("words", documentSnapshot.data());
+    console.log("words", documentSnapshot.data());
   };
 
   render() {
--- ios/poetTreeApp/AppDelegate.m
+++ ios/poetTreeApp/AppDelegate.m
@@ -4,13 +4,12 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
+@import Firebase;
 #import "AppDelegate.h"
 
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-@import Firebase;
 
 @implementation AppDelegate
 
